a9d4d5fe89843b71b98108a629d9afeff360f227
ShubhamSukum <shubhamsukum1722002@gmail.com>
2024-03-13 13:30:17 +0530

    refactoring code

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ import {
   createPieFor9,
 } from "./src/charts/charts.js";
 
+import {Q1Labels,
+  Q1Data,
+  Q2Labels,
+  Q2Data,
+  Q3Labels,
+  Q3Data,
+  Q4Labels,
+  Q4Data,
+  Q6Labels,
+  Q6Data,
+  Q7HistData,
+  Q7HistEdges,
+  Q8Labels,
+  Q8Data,
+  Q9values,
+  Q10Labels,
+  Q10Data} from "./src/data/sampleDataTesting.js";
+
 import { createFight } from "./src/fights/fights.js";
 import { Q5values } from "./src/data/sampleDataTesting.js";
 
@@ -63,7 +81,7 @@ mongoose
 
     // ************************************ //
     // QNo5 [ PIEs ]
-    createPieFor5(Q5values);     // Call this method
+    // createPieFor5(Q5values);     // Call this method
     // QNo5 [ PIEs ]
     // ************************************ //
 
@@ -117,92 +135,3 @@ mongoose
     console.log("DB Connection Failed !!");
     console.log(error);
   });
-
-const Q1Labels = [
-  "weapon_ak47",
-  "weapon_m4a1_silencer",
-  "weapon_awp",
-  "weapon_usp_silencer",
-  "weapon_mp9",
-  "weapon_deagle",
-  "weapon_mac10",
-  "weapon_galilar",
-  "weapon_m4a1",
-  "weapon_glock",
-  "weapon_p250",
-  "weapon_famas",
-  "weapon_elite",
-  "weapon_tec9",
-  "weapon_fiveseven",
-];
-const Q1Data = [
-  6170, 5536, 3089, 1922, 1898, 1143, 1066, 904, 881, 845, 671, 401, 331, 255,
-  58,
-];
-
-const Q2Labels = [
-  "CHEST",
-  "HEAD",
-  "STOMACH",
-  "RIGHTARM",
-  "RIGHTLEG",
-  "LEFTARM",
-  "LEFTLEG",
-  "GEAR",
-];
-const Q2Data = [251, 91, 81, 40, 16, 13, 8, 2];
-
-const Q3Labels = ["ar", "pistol", "sniper", "smg"];
-const Q3Data = [13892.0, 5225.0, 3089.0, 2964.0];
-
-const Q4Labels = ["No Movement", "Crouching"];
-const Q4Data = [26349.0, 11632.0];
-
-const Q6Labels = ["Obstructed Shots", "Non-Obstructed Shots"];
-const Q6Data = [2957, 35024];
-
-const Q7HistData = [
-  54, 5, 48, 20, 17, 6, 7, 3, 4, 3, 3, 80, 140, 247, 128, 364, 520, 787, 1248,
-  1397, 2741, 3569, 4514, 3982, 4268, 2649, 2474, 1847, 1608, 1266, 1131, 713,
-  595, 324, 253, 159, 72, 48, 66, 59, 66, 50, 1, 34, 9, 5, 5, 12, 33, 20, 13,
-  10, 20, 11, 3, 8, 1, 5, 4, 3,
-];
-const Q7HistEdges = [
-  -22.4395751953125, -21.4395751953125, -20.4395751953125, -19.4395751953125,
-  -18.4395751953125, -17.4395751953125, -16.4395751953125, -15.4395751953125,
-  -14.4395751953125, -13.4395751953125, -12.4395751953125, -11.4395751953125,
-  -10.4395751953125, -9.4395751953125, -8.4395751953125, -7.4395751953125,
-  -6.4395751953125, -5.4395751953125, -4.4395751953125, -3.4395751953125,
-  -2.4395751953125, -1.4395751953125, -0.4395751953125, 0.5604248046875,
-  1.5604248046875, 2.5604248046875, 3.5604248046875, 4.5604248046875,
-  5.5604248046875, 6.5604248046875, 7.5604248046875, 8.5604248046875,
-  9.5604248046875, 10.5604248046875, 11.5604248046875, 12.5604248046875,
-  13.5604248046875, 14.5604248046875, 15.5604248046875, 16.5604248046875,
-  17.5604248046875, 18.5604248046875, 19.5604248046875, 20.5604248046875,
-  21.5604248046875, 22.5604248046875, 23.5604248046875, 24.5604248046875,
-  25.5604248046875, 26.5604248046875, 27.5604248046875, 28.5604248046875,
-  29.5604248046875, 30.5604248046875, 31.5604248046875, 32.5604248046875,
-  33.5604248046875, 34.5604248046875, 35.5604248046875, 36.5604248046875,
-];
-
-const Q8Labels = [
-  "Average Utility Damage Done",
-  "Average Support Utility Used",
-];
-const Q8Data = [4.058597476942648, 0.6611099332131878];
-
-const Q9values = {
-  sniper: {
-    labels: ["Scoping", "Not Scoping"],
-    data: [0, 22],
-  },
-  scopingAR: {
-    labels: ["Scoping", "Not Scoping"],
-    data: [0, 0],
-  },
-};
-
-const Q10Labels = ["Blind Shots", "Not Blind Shots"];
-const Q10Data = [61, 37540];
-
-// ***************************************************
--- src/data/sampleDataTesting.js
+++ src/data/sampleDataTesting.js
@@ -1,87 +1,173 @@
 export const Q5values = [
-    // Sample data for weapon_category_ar
-    {
-      weapon_category: "weapon_category_ar",
-      weapons: {
-        weapon_ak47: {
-          data: [0, 0, 0, 7, 27, 96],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_famas: {
-          data: [0, 0, 0, 0, 0, 8],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_galilar: {
-          data: [0, 0, 0, 1, 16, 5],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_m4a1: {
-          data: [0, 0, 0, 1, 7, 12],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_m4a1_silencer: {
-          data: [0, 0, 2, 4, 22, 90],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
+  // Sample data for weapon_category_ar
+  {
+    weapon_category: "weapon_category_ar",
+    weapons: {
+      weapon_ak47: {
+        data: [0, 0, 0, 7, 27, 96],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_famas: {
+        data: [0, 0, 0, 0, 0, 8],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_galilar: {
+        data: [0, 0, 0, 1, 16, 5],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_m4a1: {
+        data: [0, 0, 0, 1, 7, 12],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_m4a1_silencer: {
+        data: [0, 0, 2, 4, 22, 90],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
       },
     },
-    // Sample data for weapon_category_pistol
-    {
-      weapon_category: "weapon_category_pistol",
-      weapons: {
-        weapon_deagle: {
-          data: [0, 0, 0, 0, 3, 11],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_elite: {
-          data: [0, 0, 0, 1, 2, 2],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_fiveseven: {
-          data: [0, 0, 0, 0, 0, 2],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_glock: {
-          data: [0, 0, 0, 6, 4, 8],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_p250: {
-          data: [0, 0, 0, 0, 2, 17],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_tec9: {
-          data: [0, 0, 2, 5, 1, 2],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_usp_silencer: {
-          data: [0, 0, 1, 5, 4, 22],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
+  },
+  // Sample data for weapon_category_pistol
+  {
+    weapon_category: "weapon_category_pistol",
+    weapons: {
+      weapon_deagle: {
+        data: [0, 0, 0, 0, 3, 11],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_elite: {
+        data: [0, 0, 0, 1, 2, 2],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_fiveseven: {
+        data: [0, 0, 0, 0, 0, 2],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_glock: {
+        data: [0, 0, 0, 6, 4, 8],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_p250: {
+        data: [0, 0, 0, 0, 2, 17],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_tec9: {
+        data: [0, 0, 2, 5, 1, 2],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_usp_silencer: {
+        data: [0, 0, 1, 5, 4, 22],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
       },
     },
-    // Sample data for weapon_category_smg
-    {
-      weapon_category: "weapon_category_smg",
-      weapons: {
-        weapon_mac10: {
-          data: [0, 0, 2, 1, 8, 16],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
-        weapon_mp9: {
-          data: [0, 0, 1, 17, 19, 18],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
+  },
+  // Sample data for weapon_category_smg
+  {
+    weapon_category: "weapon_category_smg",
+    weapons: {
+      weapon_mac10: {
+        data: [0, 0, 2, 1, 8, 16],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
+      },
+      weapon_mp9: {
+        data: [0, 0, 1, 17, 19, 18],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
       },
     },
-    // Sample data for weapon_category_sniper
-    {
-      weapon_category: "weapon_category_sniper",
-      weapons: {
-        weapon_awp: {
-          data: [0, 0, 0, 0, 2, 20],
-          labels: ['0-50', '50-100', '100-200', '200-300', '300-500', '500+'],
-        },
+  },
+  // Sample data for weapon_category_sniper
+  {
+    weapon_category: "weapon_category_sniper",
+    weapons: {
+      weapon_awp: {
+        data: [0, 0, 0, 0, 2, 20],
+        labels: ["0-50", "50-100", "100-200", "200-300", "300-500", "500+"],
       },
     },
-  ];
-  
+  },
+];
+
+export const Q1Labels = [
+  "weapon_ak47",
+  "weapon_m4a1_silencer",
+  "weapon_awp",
+  "weapon_usp_silencer",
+  "weapon_mp9",
+  "weapon_deagle",
+  "weapon_mac10",
+  "weapon_galilar",
+  "weapon_m4a1",
+  "weapon_glock",
+  "weapon_p250",
+  "weapon_famas",
+  "weapon_elite",
+  "weapon_tec9",
+  "weapon_fiveseven",
+];
+export const Q1Data = [
+  6170, 5536, 3089, 1922, 1898, 1143, 1066, 904, 881, 845, 671, 401, 331, 255,
+  58,
+];
+
+export const Q2Labels = [
+  "CHEST",
+  "HEAD",
+  "STOMACH",
+  "RIGHTARM",
+  "RIGHTLEG",
+  "LEFTARM",
+  "LEFTLEG",
+  "GEAR",
+];
+export const Q2Data = [251, 91, 81, 40, 16, 13, 8, 2];
+
+export const Q3Labels = ["ar", "pistol", "sniper", "smg"];
+export const Q3Data = [13892.0, 5225.0, 3089.0, 2964.0];
+
+export const Q4Labels = ["No Movement", "Crouching"];
+export const Q4Data = [26349.0, 11632.0];
+
+export const Q6Labels = ["Obstructed Shots", "Non-Obstructed Shots"];
+export const Q6Data = [2957, 35024];
+
+export const Q7HistData = [
+  54, 5, 48, 20, 17, 6, 7, 3, 4, 3, 3, 80, 140, 247, 128, 364, 520, 787, 1248,
+  1397, 2741, 3569, 4514, 3982, 4268, 2649, 2474, 1847, 1608, 1266, 1131, 713,
+  595, 324, 253, 159, 72, 48, 66, 59, 66, 50, 1, 34, 9, 5, 5, 12, 33, 20, 13,
+  10, 20, 11, 3, 8, 1, 5, 4, 3,
+];
+export const Q7HistEdges = [
+  -22.4395751953125, -21.4395751953125, -20.4395751953125, -19.4395751953125,
+  -18.4395751953125, -17.4395751953125, -16.4395751953125, -15.4395751953125,
+  -14.4395751953125, -13.4395751953125, -12.4395751953125, -11.4395751953125,
+  -10.4395751953125, -9.4395751953125, -8.4395751953125, -7.4395751953125,
+  -6.4395751953125, -5.4395751953125, -4.4395751953125, -3.4395751953125,
+  -2.4395751953125, -1.4395751953125, -0.4395751953125, 0.5604248046875,
+  1.5604248046875, 2.5604248046875, 3.5604248046875, 4.5604248046875,
+  5.5604248046875, 6.5604248046875, 7.5604248046875, 8.5604248046875,
+  9.5604248046875, 10.5604248046875, 11.5604248046875, 12.5604248046875,
+  13.5604248046875, 14.5604248046875, 15.5604248046875, 16.5604248046875,
+  17.5604248046875, 18.5604248046875, 19.5604248046875, 20.5604248046875,
+  21.5604248046875, 22.5604248046875, 23.5604248046875, 24.5604248046875,
+  25.5604248046875, 26.5604248046875, 27.5604248046875, 28.5604248046875,
+  29.5604248046875, 30.5604248046875, 31.5604248046875, 32.5604248046875,
+  33.5604248046875, 34.5604248046875, 35.5604248046875, 36.5604248046875,
+];
+
+export const Q8Labels = [
+  "Average Utility Damage Done",
+  "Average Support Utility Used",
+];
+export const Q8Data = [4.058597476942648, 0.6611099332131878];
+
+export const Q9values = {
+  sniper: {
+    labels: ["Scoping", "Not Scoping"],
+    data: [0, 22],
+  },
+  scopingAR: {
+    labels: ["Scoping", "Not Scoping"],
+    data: [0, 0],
+  },
+};
+
+export const Q10Labels = ["Blind Shots", "Not Blind Shots"];
+export const Q10Data = [61, 37540];
